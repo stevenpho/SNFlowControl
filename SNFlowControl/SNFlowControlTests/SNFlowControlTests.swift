a96133a1a10582367ff0b87540bdabcccfcdb88c
Steven <>
2025-05-05 01:24:03 +0800

    update

--- SNFlowControl/SNFlowControlTests/SNFlowControlTests.swift
+++ SNFlowControl/SNFlowControlTests/SNFlowControlTests.swift
@@ -5,13 +5,13 @@
 //  Created by Lee Steve on 2025/5/5.
 //
 
-import Testing
-@testable import SNFlowControl
-
-struct SNFlowControlTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
-}
+//import Testing
+//@testable import SNFlowControl
+//
+//struct SNFlowControlTests {
+//
+//    @Test func example() async throws {
+//        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+//    }
+//
+//}
