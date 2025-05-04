701d787dd26536173040567b269bd308228e0486
Steven <>
2025-05-05 00:34:52 +0800

    init project

---
+++ SNFlowControl/SNFlowControl/SNFlowControl.h
@@ -0,0 +1,18 @@
+//
+//  SNFlowControl.h
+//  SNFlowControl
+//
+//  Created by Lee Steve on 2025/5/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SNFlowControl.
+FOUNDATION_EXPORT double SNFlowControlVersionNumber;
+
+//! Project version string for SNFlowControl.
+FOUNDATION_EXPORT const unsigned char SNFlowControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SNFlowControl/PublicHeader.h>
+
+
---
+++ SNFlowControl/SNFlowControlTests/SNFlowControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  SNFlowControlTests.swift
+//  SNFlowControlTests
+//
+//  Created by Lee Steve on 2025/5/5.
+//
+
+import Testing
+@testable import SNFlowControl
+
+struct SNFlowControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
